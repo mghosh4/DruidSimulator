b4be80028c6273c888015c2c49dd7a06a4956eaf
flint-stone <sharonxu65@gmail.com>
2015-10-22 16:00:38 -0500

    m

--- ML-Simulator/node/Broker.py
+++ ML-Simulator/node/Broker.py
@@ -1,3 +1,19 @@
 from Node import Node
 
 class Broker(Node):     
+	
+	def timeCalculation(self, HistNodeList, queryList):
+		timeMap = []
+		for query in queryList:
+			for segment in query.segmentList:
+				for node in HistNodeList:
+					if node.lookup(segment.time) == true:
+						timeMap[node.id]++
+						
+		
+		maxscore=0
+		for node in HistNodeList:
+			if(timeMap[node.id]>=maxscore):
+				maxscore=timeMap[node.id]
+		
+		return maxscore
