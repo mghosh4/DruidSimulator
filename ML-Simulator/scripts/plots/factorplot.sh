9f2e4abad2f966a0120d465fb42930c176bd1f85
Mainak Ghosh <mghosh4@node-1.druid2.dcsq.emulab.net>
2016-03-05 17:27:03 -0700

    Plot Scripts added

--- ML-Simulator/scripts/plots/factorplot.sh
+++ ML-Simulator/scripts/plots/factorplot.sh
@@ -5,7 +5,6 @@ set output "$2"
  
 set xlabel "Time"
 set ylabel "Number of Replicas"
-set y2label "Percentage Improvement"
 set style line 1 lt rgb "#A00000" pt 1 pi 10 lw 2 ps 2
 set style line 2 lt rgb "#00A000" pt 7 pi 10 lw 2 ps 2
 set style line 3 lt rgb "#5060D0" pt 2 pi 10 lw 2 ps 2
@@ -14,13 +13,11 @@ set style line 5 lt rgb "#ED0CCB" pt 9 pi 10 lw 2 ps 2
 set autoscale                        # scale axes automatically
 set xtic auto                        # set xtics automatically
 set ytic auto                        # set ytics automatically
-set y2tic auto
 set xrange [0:1000]
-set y2range [0:100]
+set yrange [0:3]
 set key top left
 plot    "$1" using 1:2 title 'Fixed' with linespoints ls 1, \
         '' using 1:3 title 'Tiered' with linespoints ls 2, \
         '' using 1:4 title 'Adaptive' with linespoints ls 3, \
-        '' using 1:5 title 'Best Fit' with linespoints ls 5, \
-	'' using 1:6 notitle axes x1y2 with impulses ls 4
+        '' using 1:5 title 'Best Fit' with linespoints ls 5
 EOF
---
+++ ML-Simulator/scripts/plots/loads.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+gnuplot << EOF
+set terminal pngcairo font 'Times,20' linewidth 2 size 15in,9in 
+set output "$2"
+
+set style data histograms
+set style fill pattern 1 border lt -1
+set ylabel "Number of Segment Loads"
+set style line 1 lt rgb "#A00000" pt 1 pi 10 lw 2 ps 2
+set style line 2 lt rgb "#00A000" pt 7 pi 10 lw 2 ps 2
+set style line 3 lt rgb "#5060D0" pt 2 pi 10 lw 2 ps 2
+set style line 4 lt rgb "#F25900" pt 5 pi 10 lw 2 ps 2
+set style line 5 lt rgb "#ED0CCB" pt 9 pi 10 lw 2 ps 2
+set autoscale                        # scale axes automatically
+unset xtics
+set ytic auto                        # set ytics automatically
+set yrange [0:3500]
+plot    "$1" using 2 ti 'Fixed', \
+        '' using 3 ti 'Tiered', \
+        '' using 4 ti 'Adaptive', \
+        '' using 5 ti 'Best Fit'
+EOF
