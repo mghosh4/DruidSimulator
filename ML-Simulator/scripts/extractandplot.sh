ddee0af8c8b06826d1affe1b28a028cf2865e6ff
Mainak Ghosh <mghosh4@node-1.getafix.dcsq.emulab.net>
2016-03-05 19:19:53 -0700

    Fixing script

--- ML-Simulator/scripts/extractandplot.sh
+++ ML-Simulator/scripts/extractandplot.sh
@@ -7,8 +7,8 @@ bash scripts/grepdata.sh Factor $RUNLOG_PATH > "$LOG_PATH"/factor.log
 bash scripts/grepdata.sh Loads $RUNLOG_PATH | tail -1 > "$LOG_PATH"/loads.log
 bash scripts/grepdata.sh Completion $RUNLOG_PATH | tail -1 > "$LOG_PATH"/completion.log
 
-bash scripts/plots/replicas.sh $LOG_PATH/replicas.log $LOG_PATH/replicas.png
+bash scripts/plots/replicaplot.sh $LOG_PATH/replicas.log $LOG_PATH/replicas.png
 bash scripts/plots/throughput.sh $LOG_PATH/throughput.log $LOG_PATH/throughput.png
-bash scripts/plots/factor.sh $LOG_PATH/factor.log $LOG_PATH/factor.png
+bash scripts/plots/factorplot.sh $LOG_PATH/factor.log $LOG_PATH/factor.png
 bash scripts/plots/loads.sh $LOG_PATH/loads.log $LOG_PATH/loads.png
 bash scripts/plots/completion.sh $LOG_PATH/completion.log $LOG_PATH/completion.png
